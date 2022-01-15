891d1195e623ea56a7b5dc1246331cc99d298216
kplotka <kaska.plotka@gmail.com>
2022-01-15 13:18:03 +0100

    modified files

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
 
+    char username[100];
+    printf("podaj nazwe uzytkownika:\n");
+    scanf("%s", &username);
 
+    char dict[1000][5];
+
+    FILE* filePointer;
+    int bufferLength = 6;
+    char buffer[bufferLength];
+
+    filePointer = fopen("config/dictionary.txt", "r");
+    if (filePointer == NULL)
+    {
+        perror("Nie udalo sie otworzyc pliku config/dictionary.txt");
+        return 1;
+    }
+
+    while(fgets(buffer, bufferLength, filePointer)) {
+        printf("%s\n", buffer);
+    }
+
+    fclose(filePointer);
 
     return 0;
 }
+
