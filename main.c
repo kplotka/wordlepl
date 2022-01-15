4f449f9de3c6b3b79a58763566ecb7ef70ba5a1e
kplotka <kaska.plotka@gmail.com>
2022-01-15 16:48:33 +0100

    almost done working

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <time.h>
 
+struct attemptStats {
+    int last, best, worst, sum, count;
+};
 
 int main() {
 
@@ -14,30 +17,90 @@ int main() {
         return 1;
     }
 
+    // Oblicz wstępne statystyki dla użytkowanika.
+    // Format pliku to linie: <nazwa> <ilosc prób>\n.
+    FILE* statsFile = fopen("config/stats.txt", "a+");
+    if (statsFile == NULL)
+    {
+        perror("Nie udalo sie otworzyc pliku config/stats.txt");
+        return 1;
+    }
+
+    struct attemptStats userStats;
+    userStats.best = 0;
+    userStats.worst = 0;
+    userStats.sum = 0;
+    userStats.count = 0;
+    int bufferLength = 120;
+    char buffer[bufferLength];
+    while(fgets(buffer, bufferLength, statsFile)) {
+        if (buffer[0] == '\n') {
+            continue;
+        }
+
+        int notFoundUser = 0;
+        for (int i = 0; i<strlen(username)-1; i++) {
+            if (buffer[i] != username[i]){
+                notFoundUser = 1;
+                break;
+            }
+        }
+        if (notFoundUser == 1){
+            continue;
+        }
+
+        // Znajdz czesc ktora odpowiada za liczbe prob.
+        // start: strlen(username)-1+1
+        // end: strlen(buffer)-1
+        int sliceSize = strlen(buffer)-1-strlen(username);
+        char* numberSlice = malloc((sliceSize)*sizeof(char*));
+
+        for(int i= 0; i< sliceSize; i++) {
+            numberSlice[i] = buffer[strlen(username)+i];
+        }
+        userStats.last = atoi(numberSlice);
+
+        if(userStats.best == 0 || userStats.best > userStats.last){
+            userStats.best = userStats.last;
+        }
+
+        if(userStats.worst == 0 || userStats.worst < userStats.last){
+            userStats.worst = userStats.last;
+        }
+
+        userStats.sum += userStats.last;
+        userStats.count++;
+
+    }
+
+    if (userStats.count > 0){
+        printf("Twoje statystyki:\n Najlepszy wynik: %d\n Najgorszy wynik: %d\n Srednia: %d\n",
+               userStats.best, userStats.worst, userStats.sum/userStats.count);
+    }
+
+    // Wczytaj słownik.
     char dict[1000][5];
 
-    FILE* filePointer;
-    int bufferLength = 6;
-    char buffer[bufferLength];
-
-    filePointer = fopen("config/dictionary.txt", "r");
-    if (filePointer == NULL)
+    FILE* dictFile = fopen("config/dictionary.txt", "r");
+    if (dictFile == NULL)
     {
         perror("Nie udalo sie otworzyc pliku config/dictionary.txt");
         return 1;
     }
     int dictLength = 0;
-    while(fgets(buffer, bufferLength, filePointer)) {
-       if (buffer[0] == '\n') {
+    int bufferLength2 = 6;
+    char buffer2[bufferLength2];
+    while(fgets(buffer2, bufferLength2, dictFile)) {
+       if (buffer2[0] == '\n') {
            continue;
        }
 
        for (int i = 0; i<5; i++) {
-           dict[dictLength][i] = buffer[i];
+           dict[dictLength][i] = buffer2[i];
        }
        dictLength++;
     }
-    fclose(filePointer);
+    fclose(dictFile);
 
     int seed;
     time_t tt;
@@ -47,6 +110,7 @@ int main() {
     int target = rand() % dictLength;
 
     printf("zgadnij 5-literowe slowo (bez polskich znakow!):\n");
+    int attemps = 0;
 
     for(;;){
         char guess[100];
@@ -59,6 +123,7 @@ int main() {
             printf("wrong input, put 5 letter word\n");
             continue;
         }
+        attemps++;
 
         // TODO(kplotka): Zwaliduj slowo w slowniku, czy wgl istnieje.
 
@@ -89,13 +154,18 @@ int main() {
         printf("\n");
 
         if (correctLetters == 5){
-            printf("brawo, wygrana!");
+            printf("brawo, wygrana w %d probach!", attemps);
+
+            printf( "%s\n", username);
+            for(int i = 0; i < strlen(username)-1; i++){
+                fprintf(statsFile, "%c", username[i]);
+            }
+            fprintf(statsFile, " %d\n", attemps);
+            fclose(statsFile);
             return 0;
         }
 
     }
 
-
-    return 0;
 }
 
